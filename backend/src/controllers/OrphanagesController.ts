4b0ed8d56d560588f86e032b4c7f20b22a874956
Rafael Freire <rafaelsilvafreire2@gmail.com>
2020-10-13 14:40:43 -0300

    Created table and model for images

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -22,6 +22,8 @@ export default {
   },
 
   async create(req: Request, res: Response) {
+    console.log(req.files)
+
     const {
       name,
       latitude,
@@ -34,6 +36,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage)
 
+    const requestImages = req.files as Express.Multer.File[]
+    const images = requestImages.map((image) => {
+      return { path: image.filename }
+    })
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -42,6 +49,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images,
     })
 
     await orphanagesRepository.save(orphanage)
