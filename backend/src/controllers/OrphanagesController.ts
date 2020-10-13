cf586a13a214db5f8ea3f1e295a24e73268f89ed
Rafael Freire <rafaelsilvafreire2@gmail.com>
2020-10-13 13:59:55 -0300

    Code abstrected in routes and controllers

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,32 @@
+import { getRepository } from 'typeorm'
+import Orphanage from '../models/Orphanage'
+
+export default {
+  async create(req, res) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = req.body
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    })
+
+    await orphanagesRepository.save(orphanage)
+
+    return res.status(201).json(orphanage)
+  },
+}
